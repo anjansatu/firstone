2fd55111a84c9c460ccd398567bf0b02e131f3bc
Anjan Roy <50225919+anjansatu@users.noreply.github.com>
2025-08-28 12:54:47 +0600

    Add animated background and home page UI

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.front')
 
 @section('title', 'Welcome')
+@section('body-class', 'animated-bg')
 
 @section('content')
 <header class="header-area">
@@ -18,6 +19,32 @@
     <div class="container">
         <h1>Welcome</h1>
         <p class="lead">This page uses template assets.</p>
+        <a href="#" class="btn btn-primary mt-3">Get Started</a>
+    </div>
+</section>
+
+<section class="features-section py-5 text-center">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 mb-4">
+                <div class="p-4 shadow-sm bg-white rounded">
+                    <h3>Fast</h3>
+                    <p>Experience blazing fast performance.</p>
+                </div>
+            </div>
+            <div class="col-md-4 mb-4">
+                <div class="p-4 shadow-sm bg-white rounded">
+                    <h3>Secure</h3>
+                    <p>Your data is protected with industry-leading security.</p>
+                </div>
+            </div>
+            <div class="col-md-4 mb-4">
+                <div class="p-4 shadow-sm bg-white rounded">
+                    <h3>Reliable</h3>
+                    <p>Always available when you need it.</p>
+                </div>
+            </div>
+        </div>
     </div>
 </section>
 @endsection
